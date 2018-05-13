f11a0ef995494a9a35a15c51d630748bcd02e976
rishab2113 <rishab_nayak@yahoo.com>
2018-05-13 19:42:25 -0400

    next

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const clarifai = require('clarifai');
+const clarifaiClient = new Clarifai.App({apiKey:'a9b4dedc69974ab3aba1dbdd1fc92cf4'});
 app.use(express.static(__dirname + '/views'));
 app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
   res.render('homepage.html');
 });
-
-const server = app.listen(8000, () => {
+io.on('connection',function(socket){
+  console.log('user connected');
+    socket.on('disconnect',() => {
+      console.log('user disconnected');
+    });
+    socket.on('image', function(img){
+      clarifaiClient.models.predict(clarifai.GENERAL_MODEL,{base64: img}).then(
+        function(response){
+          for (i = 0; i<3;i++){
+            io.emit('response',response.outputs[0].data.concepts[i].name);
+          }
+        },
+        function(err){
+          console.log(err);
+        }
+      );
+    });
+});
+http.listen(8000, () => {
   console.log("View at localhost:8000");
 });
--- views/getUserMedia.js
+++ views/getUserMedia.js
@@ -1,8 +1,8 @@
 (function() {
   'use strict';
+  const socket = io();
   var video = document.querySelector('video'), canvas;
   canvas = document.getElementById('canvas');
-  photo = document.getElementById('photo');
   // use MediaDevices API
   function dataURLtoBlob(dataurl) {
       var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
@@ -23,15 +23,28 @@
         var CANVAS_WIDTH = canvas.width;
         var CANVAS_HEIGHT = canvas.height;
         var context;
+        var fps = 8;
+        var now;
+        var then = performance.now();
+        var interval = 1000/fps;
+        var delta
         context = canvas.getContext('2d');
         function drawVideoFrame(time) {
           rafId = requestAnimationFrame(drawVideoFrame);
-          context.drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-          var data = canvas.toDataURL('image/jpeg');
-          photo.setAttribute('src',data);
-          var blob = dataURLtoBlob(data);
+          now = performance.now();
+          delta = now - then;
+          if (delta > interval){
+            then = now - (delta % interval);
+            context.drawImage(video, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+            var data = canvas.toDataURL('image/jpeg');
+            var img = data.split(",")[1];
+            socket.emit('image',img);
+          }
         };
         rafId = requestAnimationFrame(drawVideoFrame);
+        socket.on('response',function(write){
+          document.getElementById('p1').innerHTML = write;
+        });
       })
       // permission denied:
       .catch(function(error) {

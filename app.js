721d7b3b0582cca81b1367d043359bddbe187ce0
rishab2113 <rishab_nayak@yahoo.com>
2018-05-13 02:47:29 -0400

    next

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
+app.use(express.static(__dirname + '/views'));
+app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('homepage.html');
 });
 
-const server = app.listen(8080, () => {
+const server = app.listen(8000, () => {
+  console.log("View at localhost:8000");
 });
